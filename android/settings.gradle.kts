748a1da2be264097560b3bd843b26f5ae728a799
githuber0114 <3518307831@qq.com>
2025-07-05 14:22:35 +0800

    调整settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,8 +1,12 @@
 // file: android/settings.gradle.kts
 
+import org.gradle.api.initialization.resolve.RepositoriesMode
+import java.util.Properties
+import java.io.File
+import java.io.FileInputStream
+
 pluginManagement {
     repositories {
-        // 只保留官方仓库
         google()
         mavenCentral()
         gradlePluginPortal()
@@ -17,10 +21,28 @@ plugins {
 
 include(":app")
 
+// VVVV  这是我们之前遗漏掉的最关键的部分 VVVV
+val localPropertiesFile = File(rootProject.projectDir, "local.properties")
+val properties = Properties()
+
+if (localPropertiesFile.exists()) {
+    FileInputStream(localPropertiesFile).use { fis ->
+        properties.load(fis)
+    }
+}
+
+val flutterSdkPath = properties.getProperty("flutter.sdk")
+if (flutterSdkPath == null) {
+    throw GradleException("Flutter SDK not found. Define location with flutter.sdk in the local.properties file.")
+}
+
+apply(from = File(flutterSdkPath, "packages/flutter_tools/gradle/app_plugin_loader.gradle"))
+// ^^^^ 这行代码会“注入”Flutter的插件，解决“找不到”的问题 ^^^^
+
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        // 只保留官方仓库
         google()
         mavenCentral()
     }
