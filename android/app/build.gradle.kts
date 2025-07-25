be351fa8e92d434d95c4b51d28cf46d1f536229b
githuber0114 <3518307831@qq.com>
2025-07-25 22:45:38 +0800

    解决版本依赖冲突

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -62,10 +62,7 @@ configurations.all {
             useVersion("1.9.0")
             because("Force specific version to resolve conflict")
         }
-        if (requested.group == "org.jetbrains.kotlin" && requested.name == "kotlin-stdlib-jdk8") {
-            useVersion("1.8.22")
-            because("Force specific version to resolve conflict")
-        }
+
         // 如果发现其他冲突，可以按下面的格式继续添加
         // if (requested.group == "group.name" && requested.name == "library-name") {
         //     useVersion("version.number")
