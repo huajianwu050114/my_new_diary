d3b9865b018710e6df054ef915687370d9e142d0
花间舞 <3518307831@qq.com>
2026-09-25 07:05:41 +0800

    build: align Android toolchain for device builds

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.example.my_new_diary"
     compileSdk = 36
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
